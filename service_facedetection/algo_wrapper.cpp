2f3c8a560639ea25d3112840b13133b68bcb43da
liqingyuan <13817189216@163.com>
2020-05-11 11:25:20 +0800

    [service_facedetection] update algo_wrapper.cpp

--- service_facedetection/algo_wrapper.cpp
+++ service_facedetection/algo_wrapper.cpp
@@ -2,14 +2,25 @@
 #include <vector>
 #include <iostream>
 #include <sys/time.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
 #include "algo_wrapper.hpp"
 
 using namespace std;
 
+DEFINE_string(fd_proto_path,
+              "/home/lqy/workshop/thrift_demo/service_facedetection/face_detection/yufacedetectnet-open-v1.prototxt",
+              "The prototxt path for face detection algorithm.");
+
+DEFINE_string(fd_model_path,
+              "/home/lqy/workshop/thrift_demo/service_facedetection/face_detection/yufacedetectnet-open-v1.caffemodel",
+              "The model path for face detection algorithm.");
+
+
 AlgoWrapper::AlgoWrapper() : fd_(NULL)
 {
-    const string prototxt = "/home/lqy/workshop/thrift_demo/service_facedetection/face_detection/yufacedetectnet-open-v1.prototxt";
-    const string model = "/home/lqy/workshop/thrift_demo/service_facedetection/face_detection/yufacedetectnet-open-v1.caffemodel";
+    const string prototxt = FLAGS_fd_proto_path;
+    const string model = FLAGS_fd_model_path;
     vector<float> mean;
     mean.push_back(103.94);
     mean.push_back(116.78);
@@ -17,6 +28,7 @@ AlgoWrapper::AlgoWrapper() : fd_(NULL)
     float thresh = 0.9;
 
     fd_ = new FaceDetection(prototxt, model, mean, thresh);
+    LOG(INFO) <<__FUNCTION__<< "create a new face detection instance" << std::endl;
 }
 
 AlgoWrapper::~AlgoWrapper()
@@ -40,7 +52,7 @@ vector<vector<float>> AlgoWrapper::AlgoImpl(const cv::Mat &img)
         long start_ts = getCurrentTime();
         detections = fd_->Detect(img);
         long end_ts = getCurrentTime();
-        std::cout <<__FUNCTION__<< "face detection time: " << end_ts - start_ts << std::endl;
+        LOG(INFO) <<__FUNCTION__<< "face detection time: " << end_ts - start_ts << std::endl;
     }
     return detections;
 }
